ea7cb3aee0cdfd8990ad3e796a604c53bbb33fc9
tomer.maimon <tomerma81@gmail.com>
2018-03-28 12:24:29 +0300

    Initialize project

---
+++ src/edu/cg/SeamsCarver.java
@@ -0,0 +1,65 @@
+package edu.cg;
+
+import java.awt.image.BufferedImage;
+
+public class SeamsCarver extends ImageProcessor {
+	
+	//MARK: An inner interface for functional programming.
+	@FunctionalInterface
+	interface ResizeOperation {
+		BufferedImage apply();
+	}
+	
+	//MARK: Fields
+	private int numOfSeams;
+	private ResizeOperation resizeOp;
+	
+	//TODO: Add some additional fields:
+	
+	
+	//MARK: Constructor
+	public SeamsCarver(Logger logger, BufferedImage workingImage,
+			int outWidth, RGBWeights rgbWeights) {
+		super(logger, workingImage, rgbWeights, outWidth, workingImage.getHeight()); 
+		
+		numOfSeams = Math.abs(outWidth - inWidth);
+		
+		if(inWidth < 2 | inHeight < 2)
+			throw new RuntimeException("Can not apply seam carving: workingImage is too small");
+		
+		if(numOfSeams > inWidth/2)
+			throw new RuntimeException("Can not apply seam carving: too many seams...");
+		
+		//Sets resizeOp with an appropriate method reference
+		if(outWidth > inWidth)
+			resizeOp = this::increaseImageWidth;
+		else if(outWidth < inWidth)
+			resizeOp = this::reduceImageWidth;
+		else
+			resizeOp = this::duplicateWorkingImage;
+		
+		//TODO: Initialize your additional fields and apply some preliminary calculations:
+		
+	}
+	
+	//MARK: Methods
+	public BufferedImage resize() {
+		return resizeOp.apply();
+	}
+	
+	//MARK: Unimplemented methods
+	private BufferedImage reduceImageWidth() {
+		//TODO: Implement this method, remove the exception.
+		throw new UnimplementedMethodException("reduceImageWidth");
+	}
+	
+	private BufferedImage increaseImageWidth() {
+		//TODO: Implement this method, remove the exception.
+		throw new UnimplementedMethodException("increaseImageWidth");
+	}
+	
+	public BufferedImage showSeams(int seamColorRGB) {
+		//TODO: Implement this method (bonus), remove the exception.
+		throw new UnimplementedMethodException("showSeams");
+	}
+}
