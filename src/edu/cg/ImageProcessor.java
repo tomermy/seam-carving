b65aef5a84640ff614c70950197e35d0fd7f1719
tomer.maimon <tomerma81@gmail.com>
2018-03-28 15:56:45 +0300

    Implement linear nearest neighbor

--- src/edu/cg/ImageProcessor.java
+++ src/edu/cg/ImageProcessor.java
@@ -4,166 +4,183 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 public class ImageProcessor extends FunctioalForEachLoops {
-	
-	//MARK: Fields
-	public final Logger logger;
-	public final BufferedImage workingImage;
-	public final RGBWeights rgbWeights;
-	public final int inWidth;
-	public final int inHeight;
-	public final int workingImageType;
-	public final int outWidth;
-	public final int outHeight;
-	
-	//MARK: Constructors
-	public ImageProcessor(Logger logger, BufferedImage workingImage,
-			RGBWeights rgbWeights, int outWidth, int outHeight) {
-		super(); //Initializing for each loops...
-		
-		this.logger = logger;
-		this.workingImage = workingImage;
-		this.rgbWeights = rgbWeights;
-		inWidth = workingImage.getWidth();
-		inHeight = workingImage.getHeight();
-		workingImageType = workingImage.getType();
-		this.outWidth = outWidth;
-		this.outHeight = outHeight;
-		setForEachInputParameters();
-	}
-	
-	public ImageProcessor(Logger logger,
-			BufferedImage workingImage,
-			RGBWeights rgbWeights) {
-		this(logger, workingImage, rgbWeights,
-				workingImage.getWidth(), workingImage.getHeight());
-	}
-	
-	//MARK: Change picture hue - example
-	public BufferedImage changeHue() {
-		logger.log("Prepareing for hue changing...");
-		
-		int r = rgbWeights.redWeight;
-		int g = rgbWeights.greenWeight;
-		int b = rgbWeights.blueWeight;
-		int max = rgbWeights.maxWeight;
-		
-		BufferedImage ans = newEmptyInputSizedImage();
-		
-		forEach((y, x) -> {
-			Color c = new Color(workingImage.getRGB(x, y));
-			int red = r*c.getRed() / max;
-			int green = g*c.getGreen() / max;
-			int blue = b*c.getBlue() / max;
-			Color color = new Color(red, green, blue);
-			ans.setRGB(x, y, color.getRGB());
-		});
-		
-		logger.log("Changing hue done!");
-		
-		return ans;
-	}
-	
-	
-	//MARK: Unimplemented methods
-	public BufferedImage greyscale() {
-		//TODO: Implement this method, remove the exception.
-		logger.log("Prepareing for greyscale changing...");
 
-		int r = rgbWeights.redWeight;
-		int g = rgbWeights.greenWeight;
-		int b = rgbWeights.blueWeight;
+    //MARK: Fields
+    public final Logger logger;
+    public final BufferedImage workingImage;
+    public final RGBWeights rgbWeights;
+    public final int inWidth;
+    public final int inHeight;
+    public final int workingImageType;
+    public final int outWidth;
+    public final int outHeight;
 
-		BufferedImage ans = newEmptyInputSizedImage();
+    //MARK: Constructors
+    public ImageProcessor(Logger logger, BufferedImage workingImage,
+                          RGBWeights rgbWeights, int outWidth, int outHeight) {
+        super(); //Initializing for each loops...
 
-		forEach((y, x) -> {
-			Color c = new Color(workingImage.getRGB(x, y));
-			int weightedRed = r*c.getRed();
-			int weightedGreen = g*c.getGreen();
-			int weightedBlue = b*c.getBlue();
-			int greyColor = (weightedRed + weightedGreen + weightedBlue) / (r + g + b);
-			Color resultColor = new Color(greyColor, greyColor, greyColor);
-			ans.setRGB(x, y, resultColor.getRGB());
-		});
+        this.logger = logger;
+        this.workingImage = workingImage;
+        this.rgbWeights = rgbWeights;
+        inWidth = workingImage.getWidth();
+        inHeight = workingImage.getHeight();
+        workingImageType = workingImage.getType();
+        this.outWidth = outWidth;
+        this.outHeight = outHeight;
+        setForEachInputParameters();
+    }
 
-		logger.log("Changing greyscale done!");
+    public ImageProcessor(Logger logger,
+                          BufferedImage workingImage,
+                          RGBWeights rgbWeights) {
+        this(logger, workingImage, rgbWeights,
+                workingImage.getWidth(), workingImage.getHeight());
+    }
 
-		return ans;
-	}
+    //MARK: Change picture hue - example
+    public BufferedImage changeHue() {
+        logger.log("Preparing for hue changing...");
 
-	public BufferedImage gradientMagnitude() {
-		//TODO: Implement this method, remove the exception.
-		logger.log("Prepareing for greyscale changing...");
+        int r = rgbWeights.redWeight;
+        int g = rgbWeights.greenWeight;
+        int b = rgbWeights.blueWeight;
+        int max = rgbWeights.maxWeight;
 
-		BufferedImage greyScaledImage = greyscale();
-		BufferedImage ans = greyscale();
+        BufferedImage ans = newEmptyInputSizedImage();
 
-		forEach((y, x) -> {
-			Color currentPixel = new Color(greyScaledImage.getRGB(x, y));
-			Color nextHorizontalPixel = new Color(greyScaledImage
-					.getRGB(x == (greyScaledImage.getWidth() - 1) ? x - 1 : x + 1, y));
+        forEach((y, x) -> {
+            Color c = new Color(workingImage.getRGB(x, y));
+            int red = r * c.getRed() / max;
+            int green = g * c.getGreen() / max;
+            int blue = b * c.getBlue() / max;
+            Color color = new Color(red, green, blue);
+            ans.setRGB(x, y, color.getRGB());
+        });
 
-			Color nextVerticalPixel = new Color(greyScaledImage
-					.getRGB(x, y == (greyScaledImage.getHeight() - 1) ? y - 1 : y + 1));
+        logger.log("Changing hue done!");
 
-			int currGreyColor = currentPixel.getRed(); // RGB values are equal on grey
-			int horizontalGreyColor = nextHorizontalPixel.getRed();
-			int verticalGreyColor = nextVerticalPixel.getRed();
-
-			int dxSquare = (int)Math.pow(currGreyColor - horizontalGreyColor, 2);
-			int dySquare = (int)Math.pow(currGreyColor - verticalGreyColor, 2);
-
-			int gradientMagnitude = (int)Math.sqrt((dxSquare + dySquare) / 2);
-			Color resultColor = new Color(gradientMagnitude, gradientMagnitude, gradientMagnitude);
-
-			ans.setRGB(x, y, resultColor.getRGB());
-		});
-
-		logger.log("gradient magnitude done!");
-
-		return ans;
-	}
+        return ans;
+    }
 
 
-	
-	public BufferedImage nearestNeighbor() {
-		//TODO: Implement this method, remove the exception.
-		throw new UnimplementedMethodException("nearestNeighbor");
-	}
-	
-	public BufferedImage bilinear() {
-		//TODO: Implement this method, remove the exception.
-		throw new UnimplementedMethodException("bilinear");
-	}
-	
-	
-	//MARK: Utilities
-	public final void setForEachInputParameters() {
-		setForEachParameters(inWidth, inHeight);
-	}
-	
-	public final void setForEachOutputParameters() {
-		setForEachParameters(outWidth, outHeight);
-	}
-	
-	public final BufferedImage newEmptyInputSizedImage() {
-		return newEmptyImage(inWidth, inHeight);
-	}
-	
-	public final BufferedImage newEmptyOutputSizedImage() {
-		return newEmptyImage(outWidth, outHeight);
-	}
-	
-	public final BufferedImage newEmptyImage(int width, int height) {
-		return new BufferedImage(width, height, workingImageType);
-	}
-	
-	public final BufferedImage duplicateWorkingImage() {
-		BufferedImage output = newEmptyInputSizedImage();
-		
-		forEach((y, x) -> 
-			output.setRGB(x, y, workingImage.getRGB(x, y))
-		);
-		
-		return output;
-	}
+    //MARK: Unimplemented methods
+    public BufferedImage greyscale() {
+        //TODO: Implement this method, remove the exception.
+        logger.log("Preparing for greyscale changing...");
+
+        int r = rgbWeights.redWeight;
+        int g = rgbWeights.greenWeight;
+        int b = rgbWeights.blueWeight;
+
+        BufferedImage ans = newEmptyInputSizedImage();
+
+        forEach((y, x) -> {
+            Color c = new Color(workingImage.getRGB(x, y));
+            int weightedRed = r * c.getRed();
+            int weightedGreen = g * c.getGreen();
+            int weightedBlue = b * c.getBlue();
+            int greyColor = (weightedRed + weightedGreen + weightedBlue) / (r + g + b);
+            Color resultColor = new Color(greyColor, greyColor, greyColor);
+            ans.setRGB(x, y, resultColor.getRGB());
+        });
+
+        logger.log("Changing greyscale done!");
+
+        return ans;
+    }
+
+    public BufferedImage gradientMagnitude() {
+        //TODO: Implement this method, remove the exception.
+        logger.log("Preparing for greyscale changing...");
+
+        BufferedImage greyScaledImage = greyscale();
+        BufferedImage ans = greyscale();
+
+        forEach((y, x) -> {
+            Color currentPixel = new Color(greyScaledImage.getRGB(x, y));
+            Color nextHorizontalPixel = new Color(greyScaledImage
+                    .getRGB(x == (greyScaledImage.getWidth() - 1) ? x - 1 : x + 1, y));
+
+            Color nextVerticalPixel = new Color(greyScaledImage
+                    .getRGB(x, y == (greyScaledImage.getHeight() - 1) ? y - 1 : y + 1));
+
+            int currGreyColor = currentPixel.getRed(); // RGB values are equal on grey
+            int horizontalGreyColor = nextHorizontalPixel.getRed();
+            int verticalGreyColor = nextVerticalPixel.getRed();
+
+            int dxSquare = (int) Math.pow(currGreyColor - horizontalGreyColor, 2);
+            int dySquare = (int) Math.pow(currGreyColor - verticalGreyColor, 2);
+
+            int gradientMagnitude = (int) Math.sqrt((dxSquare + dySquare) / 2);
+            Color resultColor = new Color(gradientMagnitude, gradientMagnitude, gradientMagnitude);
+
+            ans.setRGB(x, y, resultColor.getRGB());
+        });
+
+        logger.log("gradient magnitude done!");
+
+        return ans;
+    }
+
+
+    public BufferedImage nearestNeighbor() {
+        //TODO: Implement this method, remove the exception.
+        logger.log("Preparing for nearest neighbor resize...");
+        setForEachOutputParameters();
+        BufferedImage ans = newEmptyOutputSizedImage();
+
+        forEach((y, x) -> {
+            int sourceX = (int) Math.round(((double) x) / ans.getWidth() * workingImage.getWidth());
+            int sourceY = (int) Math.round(((double) y) / ans.getHeight() * workingImage.getHeight());
+
+            // Handle boundaries case
+            sourceX = Math.min(sourceX, workingImage.getWidth() - 1);
+            sourceY = Math.min(sourceY, workingImage.getHeight() - 1);
+
+            int nearestNeighborRGB = workingImage.getRGB(sourceX, sourceY);
+            ans.setRGB(x, y, nearestNeighborRGB);
+
+        });
+
+        logger.log("nearest neighbor resize done!");
+
+        return ans;
+    }
+
+    public BufferedImage bilinear() {
+        //TODO: Implement this method, remove the exception.
+        throw new UnimplementedMethodException("bilinear");
+    }
+
+    //MARK: Utilities
+    public final void setForEachInputParameters() {
+        setForEachParameters(inWidth, inHeight);
+    }
+
+    public final void setForEachOutputParameters() {
+        setForEachParameters(outWidth, outHeight);
+    }
+
+    public final BufferedImage newEmptyInputSizedImage() {
+        return newEmptyImage(inWidth, inHeight);
+    }
+
+    public final BufferedImage newEmptyOutputSizedImage() {
+        return newEmptyImage(outWidth, outHeight);
+    }
+
+    public final BufferedImage newEmptyImage(int width, int height) {
+        return new BufferedImage(width, height, workingImageType);
+    }
+
+    public final BufferedImage duplicateWorkingImage() {
+        BufferedImage output = newEmptyInputSizedImage();
+
+        forEach((y, x) ->
+                output.setRGB(x, y, workingImage.getRGB(x, y))
+        );
+
+        return output;
+    }
 }
